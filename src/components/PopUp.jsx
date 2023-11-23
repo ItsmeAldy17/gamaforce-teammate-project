e533f0300572d3c5048ef7c7df154925ff6109fa
eben4ya <benaya.imanuela@gmail.com>
2023-11-23 17:27:34 +0700

    <feat: add list mission>

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,31 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const PopUp = ({ onClose }) => {
+  const [missions, setMissions] = useState(
+    JSON.parse(localStorage.getItem("missions")) || []
+  );
   const [missionName, setMissionName] = useState("");
-  const [missionCreated, setMissionCreated] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("missions", JSON.stringify(missions));
+  }, [missions]);
 
   const handleCreateMission = () => {
-    console.log("Mission created:", missionName);
-
-    // Tampilkan PopUp berhasil jika misi berhasil dibuat
-    setMissionCreated(true);
-
-    // Sembunyikan PopUp pembuatan misi setelah beberapa detik (misalnya, 3 detik)
-    setTimeout(() => {
-      setMissionCreated(false);
-      onClose();
-    }, 3000);
+    if (missionName.trim() !== "") {
+      const newMission = { name: missionName };
+      setMissions([...missions, newMission]);
+      setMissionName("");
+    }
   };
 
-  const handleMissionCreatedClose = () => {
-    setMissionCreated(false);
+  const handleEditMission = (index) => {
+    setMissionName(missions[index].name);
+    setEditMode(true);
+    setEditIndex(index);
+  };
+
+  const handleUpdateMission = () => {
+    if (missionName.trim() !== "" && editIndex !== null) {
+      const updatedMissions = [...missions];
+      updatedMissions[editIndex].name = missionName;
+      setMissions(updatedMissions);
+      setMissionName("");
+      setEditMode(false);
+      setEditIndex(null);
+    }
+  };
+
+  const handleDeleteMission = (index) => {
+    const updatedMissions = missions.filter((_, i) => i !== index);
+    setMissions(updatedMissions);
+  };
+
+  const handleClose = () => {
+    setMissionName("");
+    setEditMode(false);
+    setEditIndex(null);
     onClose();
   };
 
   return (
     <section className="fixed top-0 z-[9999999] left-0 w-screen h-screen flex items-center justify-center bg-opacity-50 bg-gray-800 text-white">
-      <div className={`bg-white p-4 rounded-md text-black ${missionCreated ? 'hidden' : ''}`}>
-        <h2 className="text-2xl font-bold mb-4">Create Mission</h2>
+      <form  className="bg-white p-4 rounded-md text-black">
+        <h2 className="text-2xl font-bold mb-4">
+          {editMode ? "Edit Mission" : "Create Mission"}
+        </h2>
         <label className="block mb-2">Mission Name:</label>
         <input
           type="text"
@@ -34,34 +63,51 @@ const PopUp = ({ onClose }) => {
           className="w-full px-4 py-2 rounded-md border border-gray-300 mb-4"
         />
         <div className="flex justify-end">
+          {editMode ? (
+            <button
+              onClick={handleUpdateMission}
+              className="mr-2 bg-blue-500 text-white px-4 py-2 rounded-md"
+            >
+              Update
+            </button>
+          ) : (
+            <button
+              onClick={handleCreateMission}
+              className="mr-2 bg-blue-500 text-white px-4 py-2 rounded-md"
+            >
+              Create
+            </button>
+          )}
           <button
-            onClick={handleCreateMission}
-            className="mr-2 bg-blue-500 text-white px-4 py-2 rounded-md"
-          >
-            Create
-          </button>
-          <button
-            onClick={onClose}
+            onClick={handleClose}
             className="bg-gray-500 text-white px-4 py-2 rounded-md"
           >
             Cancel
           </button>
         </div>
+      </form>
+      <div className="mt-4 bg-white p-4 rounded-md text-black">
+        <h2 className="text-2xl font-bold mb-4">Missions List</h2>
+        <ul>
+          {missions.map((mission, index) => (
+            <li key={index} className="mb-2">
+              {mission.name}
+              <button
+                onClick={() => handleEditMission(index)}
+                className="ml-2 bg-yellow-500 text-white px-2 py-1 rounded-md"
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => handleDeleteMission(index)}
+                className="ml-2 bg-red-500 text-white px-2 py-1 rounded-md"
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
-      {missionCreated && (
-        <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-opacity-50 bg-green-500 text-white">
-          <div className="bg-white p-4 rounded-md text-black">
-            <h2 className="text-2xl font-bold mb-4">Mission Created!</h2>
-            <p>Your mission &quot;{missionName}&quot; has been successfully created.</p>
-            <button
-              onClick={handleMissionCreatedClose}
-              className="mt-4 bg-green-500 text-white px-4 py-2 rounded-md"
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      )}
     </section>
   );
 };
