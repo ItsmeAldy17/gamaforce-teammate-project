c2e918683c2e1a59a749c6b0ca7b10ce0f581c2b
ItsmeAldy17 <aldyardiansyah628@gmail.com>
2023-11-25 19:50:20 +0700

    <chore: update MongoDB handle error connection>

--- src/app/config/mongodb.js
+++ src/app/config/mongodb.js
@@ -4,10 +4,11 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL, {
+    });
     console.log("Connected to MongoDB 😎");
   } catch (err) {
-    console.log(err);
+    console.error("Error connecting to MongoDB:", err);
   }
 };
 
